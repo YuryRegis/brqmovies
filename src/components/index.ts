4a887f5709e9446b97501b0d24b71ad4121246a6
YuryRegis <yury_regis@hotmail.com>
2023-11-22 18:59:26 -0300

    chore: create Box component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export {Text} from './Text';
+export {Box, TouchableOpacityBox} from './Box';
+
 
 export type {TextProps} from './Text/TextProps';
+export type {BoxProps, TouchableOpacityBoxProps} from './Box';
