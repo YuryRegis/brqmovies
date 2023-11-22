004312e81669ad988700f4bcaa52a33f3712b795
YuryRegis <yury_regis@hotmail.com>
2023-11-22 20:41:05 -0300

    chore: create FormTextInput component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export {Text} from './Text';
 export {Screen} from './Screen';
 export {TextInput} from './TextInput';
 export {Box, TouchableOpacityBox} from './Box';
+export {FormTextInput} from './Form/FormTextInput';
 
 export type {TextProps} from './Text/TextProps';
 export type {TextPresets} from './Text/TextPresets';
