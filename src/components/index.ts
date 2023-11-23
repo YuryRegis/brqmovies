5a37b0dd2e946095856b307f3349099089a9c701
YuryRegis <yury_regis@hotmail.com>
2023-11-22 22:33:19 -0300

    chore: create Button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,20 @@
 export {Text} from './Text';
 export {Icon} from './Icon';
+export {Button} from './Button';
 export {Screen} from './Screen';
 export {TextInput} from './TextInput';
 export {PasswordInput} from './PasswordInput';
 export {Box, TouchableOpacityBox} from './Box';
 export {FormTextInput} from './Form/FormTextInput';
+export {ActivityIndicator} from './ActivityIndicator';
 export {FormPasswordInput} from './Form/FormPasswordInput';
 
 
 export type {TextProps} from './Text/TextProps';
 export type {TextPresets} from './Text/TextPresets';
+export type {ButtonProps} from './Button/ButtonProps';
 export type {IconName, IconBase} from './Icon/IconProps';
 export type {BoxProps, TouchableOpacityBoxProps} from './Box';
 export type {TextInputProps} from './TextInput/TextInputProps';
 export type {PasswordInputProps} from './PasswordInput/PasswordInputProps';
+export type {ActivityIndicatorProps} from './ActivityIndicator/ActivityIndicatorProps';
