10d1d472881ca1b37e138c6cc708bed685ba091b
YuryRegis <yury_regis@hotmail.com>
2023-11-25 01:02:14 -0300

    build: add splashscreen to Android devices

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {Platform} from 'react-native';
 
 import {ThemeProvider} from '@shopify/restyle';
+import SplashScreen from 'react-native-splash-screen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
@@ -10,7 +12,14 @@ import {AuthProvider} from './src/contexts';
 
 const queryClient = new QueryClient();
 
-export const App = () => {
+export function App() {
+
+  useEffect(() => {
+    if(Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
